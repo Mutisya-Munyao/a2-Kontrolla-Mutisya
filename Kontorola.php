6e8d04f9e57ceddbb18070507ae8f9305e6609c2
Ted Mutisya <tedmutisya@gmail.com>
2020-10-29 16:17:13 +0300

    Add files via upload

---
+++ Kontorola.php
@@ -0,0 +1,78 @@
+<?php
+/**
+@NB : Do not modify array structure
+An users database
+*/
+$users = [
+["id"=>1,"name"=>"Mwega Rafiki","email" => "mwega@example.com","admin"=>1,"gender"=>0],
+["id"=>2,"name"=>"Martha","email" => "martha@example.com","admin"=>0,"gender"=>1],
+["id"=>3,"name"=>"Mary","email" => "mary@example.com","admin"=>0,"gender"=>1],
+["id"=>4,"name"=>"Lazarus","email" => "laz@example.com","admin"=>0,"gender"=>0],
+["id"=>5,"name"=>"Lasaraleen","email" => "las@example.com","admin"=>0,"gender"=>1],
+["id"=>6,"name"=>"Shasta","email" => "shasta@example.com","admin"=>1,"gender"=>0],
+["id"=>7,"name"=>"Lucy","email" => "lucy@example.com","admin"=>1,"gender"=>1],
+["id"=>8,"name"=>"Tat Hermit","email" => "hermit@example.com","admin"=>0,"gender"=>0],
+["id"=>9,"name"=>"Bree","email" => "bree@example.com","admin"=>0,"gender"=>0],
+["id"=>10,"name"=>"Kin Loon","email" => "loon@example.com","admin"=>1,"gender"=>0],
+["id"=>11,"name"=>"Hwin","email" => "hwin@example.com","admin"=>0,"gender"=>1]
+];
+
+class User {
+    private $users array(
+        ["id"=>1,"name"=>"Mwega Rafiki","email" => "mwega@example.com","admin"=>1,"gender"=>0]
+["id"=>2,"name"=>"Martha","email" => "martha@example.com","admin"=>0,"gender"=>1],
+["id"=>3,"name"=>"Mary","email" => "mary@example.com","admin"=>0,"gender"=>1],
+["id"=>4,"name"=>"Lazarus","email" => "laz@example.com","admin"=>0,"gender"=>0],
+["id"=>5,"name"=>"Lasaraleen","email" => "las@example.com","admin"=>0,"gender"=>1],
+["id"=>6,"name"=>"Shasta","email" => "shasta@example.com","admin"=>1,"gender"=>0],
+["id"=>7,"name"=>"Lucy","email" => "lucy@example.com","admin"=>1,"gender"=>1],
+["id"=>8,"name"=>"Tat Hermit","email" => "hermit@example.com","admin"=>0,"gender"=>0],
+["id"=>9,"name"=>"Bree","email" => "bree@example.com","admin"=>0,"gender"=>0],
+["id"=>10,"name"=>"Kin Loon","email" => "loon@example.com","admin"=>1,"gender"=>0],
+["id"=>11,"name"=>"Hwin","email" => "hwin@example.com","admin"=>0,"gender"=>1]
+    );
+
+    function name_email()
+{
+    return $users (1, 2);
+}
+
+list($users) = getUsers();
+
+function female()
+{
+    foreach ($users as $females) {
+        if ($females['gender'] == 1) 
+           echo $females['name'];
+}
+
+function two_names ()
+{
+    if(in_array(100, $users)) {
+        echo ;
+}
+
+public class admin(
+
+    private $users2 array(
+        ["id"=>1,"name"=>"Mwega Rafiki","email" => "mwega@example.com","admin"=>1,"gender"=>0]
+["id"=>2,"name"=>"Martha","email" => "martha@example.com","admin"=>0,"gender"=>1],
+["id"=>3,"name"=>"Mary","email" => "mary@example.com","admin"=>0,"gender"=>1],
+["id"=>4,"name"=>"Lazarus","email" => "laz@example.com","admin"=>0,"gender"=>0],
+["id"=>5,"name"=>"Lasaraleen","email" => "las@example.com","admin"=>0,"gender"=>1],
+["id"=>6,"name"=>"Shasta","email" => "shasta@example.com","admin"=>1,"gender"=>0],
+["id"=>7,"name"=>"Lucy","email" => "lucy@example.com","admin"=>1,"gender"=>1],
+["id"=>8,"name"=>"Tat Hermit","email" => "hermit@example.com","admin"=>0,"gender"=>0],
+["id"=>9,"name"=>"Bree","email" => "bree@example.com","admin"=>0,"gender"=>0],
+["id"=>10,"name"=>"Kin Loon","email" => "loon@example.com","admin"=>1,"gender"=>0],
+["id"=>11,"name"=>"Hwin","email" => "hwin@example.com","admin"=>0,"gender"=>1]
+    );
+
+    function admin(
+        
+        foreach ($users2 as $admins) {
+        if ($admins['admin'] == 1) 
+           echo $admins;
+})
+    
+)
---
+++ README.md
@@ -0,0 +1,23 @@
+# Assignment 2 : Kontorola 👣
+<img src='images/oop.png'  align="center" style="align:center;text-align:right;float:right;margin-bottom:5px;" alt="oop">
+
+## Aim 🏹
+
+- Apply OOP basics in PHP
+
+## Instructions
+
+1. Review the contents of the file named Kontorola.php, it contains an array of users which mimics a database resultSet.
+2. Create a new class named User that has the users array as a private property and any other relevant properties.
+3. In User,create class methods that will return;
+   1. Return a list of all users (name & email)
+   2. Return a list of all female users(name).
+   3. Return users with two names
+4. Create another class called Admin such that it returns all admin users when instantiated.
+5. In the file named Kontorola.php, write a few test cases for your classes.
+
+>NB : Marks will be awarded for proper submission,correctness and adhering to the OOP best practices as discussed so far in class and also explored by you during the group work.
+
+<hr>
+
+> Success 🙌
